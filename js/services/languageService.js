07a0f57a3e9f0259ed07207d1ae982a850393121
Max Kordek <me@maxkordek.com>
2016-02-06 15:21:56 +0100

    Added language switcher.

--- js/services/languageService.js
+++ js/services/languageService.js
@@ -11,7 +11,21 @@ angular.module('liskApp').service('languageService', function ($rootScope, gette
     return function (lang) {
         $rootScope.lang = $rootScope.languages[1];
 
-        $rootScope.changeLang = function () {
+        $rootScope.changeLang = function (language) {
+            switch(language) {
+                case 'de':
+                    this.lang.id = 'de';
+                    break;
+                case 'cn':
+                    this.lang.id = 'zh';
+                    break;
+                case 'en':
+                    this.lang.id = 'en';
+                    break;
+                default:
+                    this.lang.id ='en';
+            }
+
             gettextCatalog.setCurrentLanguage(this.lang.id);
             console.log('Language changed to:', this.lang.name);
         }
