295b281ed39a893fcdc4b1e40d8c923ae1d08e07
Olivier Beddows <olivier@lisk.io>
2016-02-11 19:13:57 +0000

    Adding Russian language support.

--- js/services/languageService.js
+++ js/services/languageService.js
@@ -5,6 +5,7 @@ angular.module('liskApp').service('languageService', function ($rootScope, $wind
     $rootScope.languages = [
         { id: 'en', name: 'English' },
         { id: 'de', name: 'Deutsch' },
+        { id: 'ru', name: 'русский' },
         { id: 'zh', name: '中文' }
     ];
 
