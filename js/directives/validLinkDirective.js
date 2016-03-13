b97d9997045d7178d0bf87c05d750bcaeb6558a0
Olivier Beddows <olivier@lisk.io>
2016-03-13 10:27:25 +0000

    Adding validLink directive.

--- js/directives/validLinkDirective.js
+++ js/directives/validLinkDirective.js
@@ -1,26 +1,20 @@
 require('angular');
+
 angular.module('liskApp').directive('validLink', function () {
-  return {
-    require: 'ngModel',
-    scope: {
-      extension: '@extension'
-    },
-    link: function (scope, elem, attrs, ctrl) {
-      scope.extension = (!scope.extension) ? '' : '.zip';
+    return {
+        require: 'ngModel',
+        scope: {
+            validLink: '@validLink',
+        },
+        link: function (scope, element, attrs, ctrl) {
+            scope.validLink = (!scope.validLink) ? '' : scope.validLink;
+            var regexp = new RegExp('^(http[s]?:\/\/)([a-z0-9-./]+)(' + scope.validLink + ')$', 'i');
 
-      var regexp = new RegExp('^(http[s]?:\/\/)([a-z0-9-./]+)(' + scope.extension + ')$', 'i');
+            ctrl.$validators.validLink = function (modelValue, viewValue) {
+              var value = (modelValue || viewValue);
 
-      elem.on('blur', function () {
-        scope.$apply(function () {
-          var link = elem.val().trim();
-
-          if (link.length > 0) {
-            ctrl.$setValidity('pattern', link.match(regexp) ? true : false);
-          } else {
-            ctrl.$setValidity('pattern', true);
-          }
-        });
-      });
+              return !value || value.length == 0 || regexp.test(viewValue);
+            };
+        }
     }
-  }
 });
