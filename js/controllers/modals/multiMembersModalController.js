5cccf39019356d09d42478fdd98b5bcf08e554ae
Olivier Beddows <olivier@lisk.io>
2016-03-18 01:02:39 +0000

    Revising modal title.

--- js/controllers/modals/multiMembersModalController.js
+++ js/controllers/modals/multiMembersModalController.js
@@ -2,7 +2,7 @@ require('angular');
 
 angular.module('liskApp').controller('multiMembersModalController', ["$scope", "multiMembersModal", 'gettextCatalog', function ($scope, multiMembersModal, gettextCatalog) {
 
-    $scope.title = $scope.confirmed ? gettextCatalog.getString('Confirmed by') : gettextCatalog.getString('Members');
+    $scope.title = $scope.confirmed ? gettextCatalog.getString('Confirmed by') : gettextCatalog.getString('Multi-Signature Group Members');
 
     $scope.close = function () {
         if ($scope.destroy) {
