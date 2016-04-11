7b2c4bd197539d9a20dc28e8bf5f536c27e8c919
Max Kordek <me@maxkordek.com>
2016-04-11 12:47:01 +0200

    Added dapp registration fee.

--- js/controllers/modals/addDappModalController.js
+++ js/controllers/modals/addDappModalController.js
@@ -9,6 +9,7 @@ angular.module('liskApp').controller('addDappModalController', ["$scope", "$http
     $scope.passmode = false;
     $scope.errorMessage = "";
     $scope.checkSecondPass = false;
+    $scope.fee = 500;
 
     $scope.close = function () {
         addDappModal.deactivate();
