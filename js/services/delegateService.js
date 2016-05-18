4ffbeb655af2a52dc32ac83046919802ed1b2c2a
Olivier Beddows <olivier@lisk.io>
2016-05-18 17:59:11 +0100

    Fixing spelling errors.

--- js/services/delegateService.js
+++ js/services/delegateService.js
@@ -24,7 +24,7 @@ angular.module('liskApp').service('delegateService', function ($http, $filter) {
         gettingTop: false,
         gettingVoted: false,
         cachedTOP: {data: [], time: new Date()},
-        cachedStundby: {data: [], time: new Date()},
+        cachedStandby: {data: [], time: new Date()},
         cachedVotedDelegates: {data: [], time: new Date()},
 
         isActiveRate: function (rate) {
@@ -59,8 +59,8 @@ angular.module('liskApp').service('delegateService', function ($http, $filter) {
         getStandbyList: function ($defer, params, filter, cb) {
             if (!this.gettingStandBy) {
                 this.gettingStandBy = !this.gettingStandBy;
-                if (delegates.cachedStundby.data.length > 0 && new Date() - delegates.cachedStundby.time < 1000 * 10) {
-                    var filteredData = filterData(delegates.cachedStundby.data, filter);
+                if (delegates.cachedStandby.data.length > 0 && new Date() - delegates.cachedStandby.time < 1000 * 10) {
+                    var filteredData = filterData(delegates.cachedStandby.data, filter);
                     var transformedData = sliceData(orderData(filteredData, params), params);
                     params.total(filteredData.length);
                     this.gettingStandBy = !this.gettingStandBy;
@@ -68,18 +68,18 @@ angular.module('liskApp').service('delegateService', function ($http, $filter) {
                     $defer.resolve(transformedData);
                 }
                 else {
-                    this.cachedStundby.data = [];
+                    this.cachedStandby.data = [];
                     var getPart = function (limit, offset) {
                         $http.get("/api/delegates/", {params: {orderBy: "rate:asc", limit: limit, offset: offset}})
                             .then(function (response) {
                                 if (response.data.delegates.length > 0) {
-                                    delegates.cachedStundby.data = delegates.cachedStundby.data.concat(response.data.delegates);
+                                    delegates.cachedStandby.data = delegates.cachedStandby.data.concat(response.data.delegates);
                                     getPart(limit, limit + offset);
                                 } else {
-                                    delegates.cachedStundby.time = new Date();
-                                    params.total(delegates.cachedStundby.data.length);
-                                    var filteredData = $filter('filter')(delegates.cachedStundby.data, filter);
-                                    var transformedData = transformData(delegates.cachedStundby.data, filter, params);
+                                    delegates.cachedStandby.time = new Date();
+                                    params.total(delegates.cachedStandby.data.length);
+                                    var filteredData = $filter('filter')(delegates.cachedStandby.data, filter);
+                                    var transformedData = transformData(delegates.cachedStandby.data, filter, params);
                                     delegates.gettingStandBy = !delegates.gettingStandBy;
                                     cb();
                                     $defer.resolve(transformedData);
