88b7816d77cd1dfdcbac763481ee710d7aefb63f
Olivier Beddows <olivier@lisk.io>
2016-05-24 15:20:20 +0100

    Updating directive to change input value.

--- js/directives/voteActionDirective.js
+++ js/directives/voteActionDirective.js
@@ -2,18 +2,15 @@ require('angular');
 
 angular.module('liskApp').directive('voteAction', function (gettextCatalog) {
     return {
-        restrict: 'E',
-        template: '<span>{{action}}</span>',
-        replace: true,
-        transclude: true,
+        restrict: 'A',
         scope: {
             adding: '=adding'
         },
         link: function (scope, element, attrs) {
             if (scope.adding) {
-                scope.action = gettextCatalog.getString('CONFIRM VOTE');
+                element[0].value = gettextCatalog.getString('CONFIRM VOTE');
             } else {
-                scope.action = gettextCatalog.getString('REMOVE VOTE');
+                element[0].value = gettextCatalog.getString('REMOVE VOTE');
             }
         }
     };
