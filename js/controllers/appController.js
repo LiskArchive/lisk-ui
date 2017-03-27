9bbbff5664e6910bac4732a7754a230530baff41
alihaghighatkhah <ali@lightcurve.io>
2017-03-27 15:54:11 +0200

    fixed the issue with 0%

--- js/controllers/appController.js
+++ js/controllers/appController.js
@@ -407,8 +407,8 @@ angular.module('liskApp').controller('appController', ['dappsService', '$scope',
     $scope.getSync = function () {
         $http.get("/api/loader/status/sync").then(function (resp) {
             if (resp.data.success) {
-                $scope.syncState = (resp.data.syncing && resp.data.blocks) ? Math.floor(100 * resp.data.height / resp.data.blocks) : 0;
-                if ($scope.syncState) {
+                $scope.syncState = (resp.data.syncing && resp.data.blocks) ? Math.floor(100 * resp.data.height / resp.data.blocks) : null;
+                if ($scope.syncState != undefined) {
                     $scope.loading.values = [resp.data.height, Math.abs(resp.data.blocks - resp.data.height)];
                 }
             }
